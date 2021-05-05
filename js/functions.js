72b86913eeae72a6239e241c5c3e4a1133d17133
unknown <nicholas.m.yeung@gmail.com>
2021-05-05 19:56:58 -0400

    add fade

--- js/functions.js
+++ js/functions.js
@@ -333,3 +333,19 @@ BackgroundLazyLoader();
     return delay;
   };
 }());
+
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var fades = $(".fade");
+
+  for (var i = 0; i < fades.length; i++) {
+    var fade = fades[i];
+
+    if ($(fade).position().top < pageBottom) {
+      $(fade).addClass("visible");
+    } else {
+      $(fade).removeClass("visible");
+    }
+  }
+});
